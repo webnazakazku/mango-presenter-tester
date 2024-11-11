18e916718aa4283b67b10e707e4332e7c406bb1c
Petr Parolek <petr.parolek@gmail.com>
2024-11-11 23:15:17 +0100

    fixed tests

--- tests/bootstrap.configurator.php
+++ tests/bootstrap.configurator.php
@@ -4,6 +4,8 @@ use Webnazakazku\MangoTester\Infrastructure\InfrastructureConfigurator;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+DG\BypassFinals::enable();
+
 $configurator = new InfrastructureConfigurator(__DIR__ . '/temp');
 $configurator->addConfig(__DIR__ . '/config/infrastructure.neon');
 $configurator->setupTester();
